c2339166f3daca8f620a7224d2104db722f1587f
Stathis90 <90stathis@gmail.com>
2020-06-15 13:58:53 +0300

    initial commit
    Initial Commit

---
+++ js/mcf.js
@@ -0,0 +1,292 @@
+posters = [{
+    name: "Iron Man",
+    class: "iron man ironman",
+    year: "2008",
+    description: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    link: "https://www.imdb.com/title/tt0371746/",
+    img: "ironman_poster.jpg"
+}, {
+    name: "The Incredible Hulk",
+    class: "hulk incredible",
+    year: "2008",
+    description: "Bruce Banner, a scientist on the run from the U.S. Government, must find a cure for the monster he turns into whenever he loses his temper.",
+    link: "https://www.imdb.com/title/tt0800080/",
+    img: "hulk_poster.jpg"
+}, {
+    name: "Iron Man 2",
+    class: "iron man ironman",
+    year: "2010",
+    description: "With the world now aware of his identity as Iron Man, Tony Stark must contend with both his declining health and a vengeful mad man with ties to his father's legacy.",
+    link: "https://www.imdb.com/title/tt1228705/",
+    img: "ironman2_poster.jpg"
+}, {
+    name: "Thor",
+    class: "thor",
+    year: "2011",
+    description: "The powerful but arrogant god Thor is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.",
+    link: "https://www.imdb.com/title/tt0800369/",
+    img: "thor_poster.jpg"
+}, {
+    name: "Captain America: The First Avenger",
+    class: "captain america captainamerica",
+    year: "2011",
+    description: "Steve Rogers, a rejected military soldier, transforms into Captain America after taking a dose of a Super-Soldier serum. But being Captain America comes at a price as he attempts to take down a war monger and a terrorist organization.",
+    link: "https://www.imdb.com/title/tt0458339/",
+    img: "ca_poster.jpg"
+}, {
+    name: "The Avengers",
+    class: "avengers",
+    year: "2012",
+    description: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+    link: "https://www.imdb.com/title/tt0848228/",
+    img: "avengers_poster.jpg"
+}, {
+    name: "Iron Man 3",
+    class: "iron man ironman",
+    year: "2013",
+    description: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.",
+    link: "https://www.imdb.com/title/tt1300854/",
+    img: "ironman3_poster.jpg"
+}, {
+    name: "Thor: The Dark World",
+    class: "thor",
+    year: "2013",
+    description: "When the Dark Elves attempt to plunge the universe into darkness, Thor must embark on a perilous and personal journey that will reunite him with doctor Jane Foster.",
+    link: "https://www.imdb.com/title/tt1981115/",
+    img: "thor2_poster.jpg"
+}, {
+    name: "Captain America: The Winter Soldier",
+    class: "captain america winter soldier",
+    year: "2014",
+    description: "As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat from history: an assassin known as the Winter Soldier.",
+    link: "https://www.imdb.com/title/tt1843866/",
+    img: "ca2_poster.jpg"
+}, {
+    name: "Guardians of the Galaxy",
+    class: "guardians galaxy",
+    year: "2014",
+    description: "A group of intergalactic criminals must pull together to stop a fanatical warrior with plans to purge the universe.",
+    link: "https://www.imdb.com/title/tt2015381/",
+    img: "guardians_poster.jpg"
+}, {
+    name: "Avengers: Age of Ultron",
+    class: "avengers age ultron",
+    year: "2015",
+    description: "When Tony Stark and Bruce Banner try to jump-start a dormant peacekeeping program called Ultron, things go horribly wrong and it's up to Earth's mightiest heroes to stop the villainous Ultron from enacting his terrible plan.",
+    link: "https://www.imdb.com/title/tt2395427/",
+    img: "avengers2_poster.jpg"
+}, {
+    name: "Ant-Man",
+    class: "ant man antman",
+    year: "2015",
+    description: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help his mentor, Dr. Hank Pym, plan and pull off a heist that will save the world.",
+    link: "https://www.imdb.com/title/tt0478970/",
+    img: "antman_poster.jpg"
+}, {
+    name: "Captain America: Civil War",
+    class: "iron man captain america",
+    year: "2016",
+    description: "Political involvement in the Avengers' affairs causes a rift between Captain America and Iron Man.",
+    link: "https://www.imdb.com/title/tt3498820/",
+    img: "ca3_poster.jpg"
+}, {
+    name: "Doctor Strange",
+    class: "doctor strange",
+    year: "2016",
+    description: "While on a journey of physical and spiritual healing, a brilliant neurosurgeon is drawn into the world of the mystic arts.",
+    link: "https://www.imdb.com/title/tt1211837/",
+    img: "DrStrange_poster.jpg"
+}, {
+    name: "Guardians of the Galaxy Vol. 2",
+    class: "guardians galaxy",
+    year: "2017",
+    description: "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.",
+    link: "https://www.imdb.com/title/tt3896198/",
+    img: "guardians2_poster.jpg"
+}, {
+    name: "Spider-Man: Homecoming",
+    class: "spider man homecoming",
+    year: "2017",
+    description: "Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.",
+    link: "https://www.imdb.com/title/tt2250912/",
+    img: "spiderman_poster.jpg"
+}, {
+    name: "Thor: Ragnarok",
+    class: "thor ragnarok",
+    year: "2017",
+    description: "Imprisoned on the planet Sakaar, Thor must race against time to return to Asgard and stop Ragnarök, the destruction of his world, at the hands of the powerful and ruthless villain Hela.",
+    link: "https://www.imdb.com/title/tt3501632/",
+    img: "thor3_poster.jpg"
+}, {
+    name: "Black Panther",
+    class: "black panther",
+    year: "2018",
+    description: "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.",
+    link: "https://www.imdb.com/title/tt1825683/",
+    img: "blackpanther_poster.jpg"
+}, {
+    name: "Avengers: Infinity War",
+    class: "avengers infinity war",
+    year: "2018",
+    description: "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
+    link: "https://www.imdb.com/title/tt4154756/",
+    img: "avengers3_poster.jpg"
+}, {
+    name: "Ant-Man and the Wasp",
+    class: "ant man wasp",
+    year: "2018",
+    description: "As Scott Lang balances being both a superhero and a father, Hope van Dyne and Dr. Hank Pym present an urgent new mission that finds the Ant-Man fighting alongside The Wasp to uncover secrets from their past.",
+    link: "https://www.imdb.com/title/tt5095030/",
+    img: "antman2_poster.jpg"
+}, {
+    name: "Captain Marvel",
+    class: "captain marvel",
+    year: "2019",
+    description: "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
+    link: "https://www.imdb.com/title/tt4154664/",
+    img: "cm_poster.jpg"
+}, {
+    name: "Avengers: Endgame",
+    class: "avengers endgame",
+    year: "2019",
+    description: "With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+    link: "https://www.imdb.com/title/tt4154796/",
+    img: "avengers4_poster.jpg"
+}, {
+    name: "Spider-Man: Far From Home",
+    class: "spiderman spider man",
+    year: "2019",
+    description: "Following the events of Avengers: Endgame (2019), Spider-Man must step up to take on new threats in a world that has changed forever.",
+    link: "https://www.imdb.com/title/tt6320628/",
+    img: "spiderman2_poster.jpg"
+}, {
+    name: "Agents of S.H.I.E.L.D.",
+    class: "agents shield",
+    year: "2013-2020",
+    description: "The missions of the Strategic Homeland Intervention, Enforcement and Logistics Division.",
+    link: "https://www.imdb.com/title/tt2364582/",
+    img: "agents.jpg"
+}, {
+    name: "Agent Carter",
+    class: "agent carter",
+    year: "2015-2016",
+    description: "In 1946, Peggy Carter is relegated to secretarial duties in the Strategic Scientific Reserve (SSR). When Howard Stark is accused of treason, he secretly recruits Peggy to clear his name with the help of his butler, Edwin Jarvis.",
+    link: "https://www.imdb.com/title/tt3475734/",
+    img: "ac.jpg"
+}, {
+    name: "Daredevil",
+    class: "daredevil",
+    year: "2015-2018",
+    description: "A blind lawyer by day, vigilante by night. Matt Murdock fights the crime of New York as Daredevil.",
+    link: "https://www.imdb.com/title/tt3322312/",
+    img: "daredevil.jpg"
+}, {
+    name: "Jessica Jones",
+    class: "jessica jones",
+    year: "2015-2019",
+    description: "Following the tragic end of her brief superhero career, Jessica Jones tries to rebuild her life as a private investigator, dealing with cases involving people with remarkable abilities in New York City.",
+    link: "https://www.imdb.com/title/tt2357547/",
+    img: "jessicajones.jpg"
+}, {
+    name: "Luke Cage",
+    class: "luke cage",
+    year: "2016-2018",
+    description: "When a sabotaged experiment gives him super strength and unbreakable skin, Luke Cage becomes a fugitive attempting to rebuild his life in Harlem and must soon confront his past and fight a battle for the heart of his city.",
+    link: "https://www.imdb.com/title/tt3322314/",
+    img: "lukecage.jpg"
+}, {
+    name: "Iron Fist",
+    class: "iron fist",
+    year: "2017-2018",
+    description: "A young man is bestowed with incredible martial arts skills and a mystical force known as the Iron Fist.",
+    link: "https://www.imdb.com/title/tt3322310/",
+    img: "ironfist.jpg"
+}, {
+    name: "The Defenders",
+    class: "the defenders",
+    year: "2017",
+    description: "Set a few months after the events of the second season of Daredevil, and a month after the events of Iron Fist, the vigilantes Daredevil, Jessica Jones, Luke Cage, and Iron Fist team up in New York City to fight a common enemy: The Hand.",
+    link: "https://www.imdb.com/title/tt4230076/",
+    img: "defenders.jpg"
+}, {
+    name: "The Punisher",
+    class: "the punisher",
+    year: "2017-2019",
+    description: "After the murder of his family, Marine veteran Frank Castle becomes the vigilante known as The Punisher, with only one goal in mind: to avenge them.",
+    link: "https://www.imdb.com/title/tt5675620/",
+    img: "punisher.jpg"
+}]
+
+showall() // Execute the function and show all movies and series
+
+filterSelection("all") // Execute the function and show all 
+
+function filterSelection() {
+    var x, i, input, filter;
+    x = document.getElementsByClassName("poster");
+    input = document.getElementById("search-input");
+    filter = input.value.toLowerCase();
+    if (filter == "all") filter = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "showPoster");
+        if (x[i].className.indexOf(filter) > -1) w3AddClass(x[i], "showPoster");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+
+// Show all posters for movies and series
+function showall() {
+
+    var texthtml = "";
+
+    // Sort poster array form newer to olders
+    var postersSorted = posters.sort((a, b) => (a.year < b.year) ? 1 : -1);
+
+    // Create elements for movie posters based on the array
+    for (i = 0; i < postersSorted.length; i++) {
+        texthtml += `<div class="col-sm-6 col-md-6 col-lg-3 mb-5 poster ${postersSorted[i].class}">
+                      <div class="moviesseries-item mx-auto">
+                          <a href="${postersSorted[i].link}" target="_blank">
+                              <div
+                                  class="moviesseries-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                  <div class="moviesseries-item-caption-content text-center text-white">
+                                      <h1>${postersSorted[i].name}</h1>
+                                      <p>${postersSorted[i].year}</p>
+                                      <p>${postersSorted[i].description}</p>
+                                  </div>
+                              </div>
+                          </a>
+                          <img class="img-fluid" src="assets/img/moviesseries/${postersSorted[i].img}" alt="" />
+                      </div>
+                  </div>`;
+    }
+
+    //Append elements to the specific div on the page
+    document.getElementById('allSection').innerHTML = texthtml;
+}
